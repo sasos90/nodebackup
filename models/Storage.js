7d67d1f8034db68f6abe554a0c23084729bf2d78
Saso Sabotin <saso.sabotin@scoopandspoon.com>
2016-10-14 12:13:24 +0200

    Hierarchy map created

--- models/Storage.js
+++ models/Storage.js
@@ -8,8 +8,45 @@ var storageSchema = mongoose.Schema({
     name: String,
     vendorId: String,
     productId: String,
-    backupToStorageId: String
+    backupToStorageId: String,
+    backupStorageObject: this
 });
+storageSchema.statics.findHierarchy = (callback) => {
+
+    Storage.find({
+        backupToStorageId: { $ne: null }
+    }, (err, storages) => {
+
+        assignTargets(storages, [], callback);
+    });
+};
 var Storage = mongoose.model("Storage", storageSchema);
+
+var assignTargets = (hierarchyMap, result, callback) => {
+
+    let allAssigned = true;
+
+    for (let storage of hierarchyMap) {
+        if (typeof storage.backupStorageObject === "undefined") {
+
+            // we need to find it and attach it :)
+            Storage.findById(storage.backupToStorageId, (err, item) => {
+                if (err) {
+                    console.error(err);
+                }
+
+                // assign object
+                storage.backupStorageObject = item;
+                result.push(storage);
+                assignTargets(hierarchyMap, result, callback);
+            });
+            allAssigned = false;
+            break;
+        }
+    }
+    if (allAssigned) {
+        callback(result);
+    }
+};
 
 module.exports = Storage;
--- routes/index.js
+++ routes/index.js
@@ -7,53 +7,67 @@ var db = mongoose.connection;
 /* GET home page. */
 router.get("/", (req, res, next) => {
 
-    mongoose.connect("mongodb://localhost:27017/nodebackup");
-    db.on("error", () => {
+    if (mongoose.connection.readyState === 0) {
+        var client = mongoose.connect("mongodb://localhost:27017/nodebackup");
+    }
+    db.on("error", (err) => {
+        console.error(err);
         console.error("connection error:");
     });
-    db.once("open", () => {
-        // we're connected!
-        console.log("DB connection opened now");
 
-        /*Storage.find((err, storages) => {
-            if (err) return console.error(err);
-            console.log(storages);
-        });*/
-
-        // how to update storage item
-        /*Storage.findById("58009893f3598b33102c9b5d", (err, item) => {
-            if (err) {
-                console.error(err);
-            }
-
-            // change data
-            item.backupToStorageId = "58009172bf76812d01c8604c";  // update the bears info
-
-            item.save((err) => {
-                if (err) {
-                    console.error(err);
-                }
-                console.log("!!! UPDATED !!!");
-            });
-
-        });*/
-
-        // how to create a storage item
-        /*var storageItem = new Storage({
-            name: "USB stick 1",
-            vendorId: "vendorID_1",
-            productId: "productID_1"
+    // check if database is opened
+    if (mongoose.connection.readyState === 0) {
+        db.once("open", () => {
+            // we're connected!
+            console.log("DB connection opened now");
+            render();
         });
-        storageItem.save((err, insertedItem) => {
-            if (err) {
-                console.error("NOT INSERTED");
-                return console.error(err);
-            }
-            console.log("New storage item inserted:", insertedItem);
-        });*/
-    });
+    } else {
+        render();
+    }
 
     res.render("index", { title: "Express" });
 });
+
+var render = () => {
+
+    Storage.findHierarchy((hierarchyMap) => {
+        console.log("=======");
+        console.log(hierarchyMap);
+        console.log("=======");
+    });
+
+    // how to update storage item
+    /*Storage.findById("58009893f3598b33102c9b5d", (err, item) => {
+        if (err) {
+            console.error(err);
+        }
+
+        // change data
+        item.backupToStorageId = "58009172bf76812d01c8604c";  // update the bears info
+
+        item.save((err) => {
+            if (err) {
+                console.error(err);
+            }
+            console.log("!!! UPDATED !!!");
+        });
+
+    });*/
+
+    // how to create a storage item
+    /*var storageItem = new Storage({
+        name: "USB stick 1",
+        vendorId: "vendorID_1",
+        productId: "productID_1"
+    });
+    storageItem.save((err, insertedItem) => {
+        if (err) {
+            console.error("NOT INSERTED");
+            return console.error(err);
+        }
+        console.log("New storage item inserted:", insertedItem);
+    });*/
+};
 
 module.exports = router;
