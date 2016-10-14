a24ab17dbfd01820890d240b734f0a6b34abcae5
Saso Sabotin <saso.sabotin@scoopandspoon.com>
2016-10-14 09:58:08 +0200

    Using mongoose object

--- routes/index.js
+++ routes/index.js
@@ -3,34 +3,39 @@ var router = express.Router();
 var mongoose = require("mongoose");
 var db = mongoose.connection;
 
-var collectionList = ["storages", "relations"];
+// Mongoose schemas
+var storageSchema = mongoose.Schema({
+    name: String,
+    vendorId: String,
+    productId: String
+});
+var Storage = mongoose.model("Storage", storageSchema);
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
 
     mongoose.connect("mongodb://localhost:27017/nodebackup");
-    db.on("error", function() {
-        console.error(console, "connection error:");
+    db.on("error", () => {
+        console.error("connection error:");
     });
-    db.once("open", function() {
+    db.once("open", () => {
         // we're connected!
         console.log("DB connection opened now");
+        var storageItem = new Storage({
+            name: "Ime Storage-aaaa",
+            vendorId: "vendorIDDD",
+            productId: "productIDDD"
+        });
+        storageItem.save((err, insertedItem) => {
+            if (err) {
+                console.error("NOT INSERTED");
+                return console.error(err);
+            }
+            console.log("INSERTEEED");
+        });
     });
 
     res.render("index", { title: "Express" });
 });
-
-function createCollections(db, callback, index = 0) {
-
-    if (collectionList.length !== index) {
-        db.createCollection(collectionList[index]).then(() => {
-            console.log("Created " + collectionList[index]);
-            createCollections(db, callback, ++index);
-        });
-    } else {
-        // done
-        callback();
-    }
-}
 
 module.exports = router;
