93aa0d5a46429ec3c7426dde29ae7bfe06213e58
Saso Sabotin <saso.sabotin@scoopandspoon.com>
2016-10-14 10:44:08 +0200

    Sample for updating data

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,29 @@ router.get("/", (req, res, next) => {
         // we're connected!
         console.log("DB connection opened now");
 
+        /*Storage.find((err, storages) => {
+            if (err) return console.error(err);
+            console.log(storages);
+        });*/
+
+        // how to update storage item
+        /*Storage.findById("58009893f3598b33102c9b5d", (err, item) => {
+            if (err) {
+                console.error(err);
+            }
+
+            // change data
+            item.backupToStorageId = "58009172bf76812d01c8604c";  // update the bears info
+
+            item.save((err) => {
+                if (err) {
+                    console.error(err);
+                }
+                console.log("!!! UPDATED !!!");
+            });
+
+        });*/
+
         // how to create a storage item
         /*var storageItem = new Storage({
             name: "USB stick 1",
