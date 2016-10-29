e099832b9b0b34074cef404497eea0e1da10e554
Saso Sabotin <sasos90@gmail.com>
2016-10-29 10:48:40 +0200

    Get storages data from db

--- routes/storages.js
+++ routes/storages.js
@@ -1,24 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require("mongoose");
+var Storage = require('../models/Storage');
+var db = mongoose.connection;
 
 /* GET storage listing. */
 router.get('/', (req, res, next) => {
 
-  let storageList = [
-      {
-          name: "USB A",
-          vendorId: "1234",
-          productId: "abcd"
-      }, {
-          name: "USB B",
-          vendorId: "5678",
-          productId: "efgh"
-      }
-  ];
-  res.render("storages", {
-      title: 'Storages',
-      storages: storageList
-  });
+    if (mongoose.connection.readyState === 0) {
+        var client = mongoose.connect("mongodb://localhost:27017/nodebackup");
+    }
+    db.on("error", (err) => {
+        console.error(err);
+        console.error("connection error:");
+    });
+
+    // check if database is opened
+    if (mongoose.connection.readyState === 0) {
+        db.once("open", () => {
+            // we're connected!
+            console.log("DB connection opened now");
+            render(res);
+        });
+    } else {
+        render(res);
+    }
 });
+
+var render = (res) => {
+
+    Storage.find((err, storages) => {
+        if (err) return console.error(err);
+
+        console.log(storages);
+        let storageList = storages;
+
+        res.render("storages", {
+            title: 'Storages',
+            storages: storageList
+        });
+    });
+};
 
 module.exports = router;
