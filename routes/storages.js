2c8cb5cd19e15de767eb8255b8c69f295d89b858
Saso Sabotin <saso.sabotin@scoopandspoon.com>
2016-11-02 15:05:02 +0100

    Using database like a singleton

--- routes/storages.js
+++ routes/storages.js
@@ -1,30 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require("mongoose");
-var Storage = require('../models/Storage');
-var db = mongoose.connection;
+var Storage = mongoose.model("Storage");
 
 /* GET storage listing. */
 router.get('/', (req, res, next) => {
 
-    if (mongoose.connection.readyState === 0) {
-        var client = mongoose.connect("mongodb://localhost:27017/nodebackup");
-    }
-    db.on("error", (err) => {
-        console.error(err);
-        console.error("connection error:");
-    });
-
-    // check if database is opened
-    if (mongoose.connection.readyState === 0) {
-        db.once("open", () => {
-            // we're connected!
-            console.log("DB connection opened now");
-            render(res);
-        });
-    } else {
-        render(res);
-    }
+    render(res);
 });
 
 let render = (res) => {
