2a1ec2a4059364c9c2dab6a2a73ee00d307d9b73
Saso Sabotin <sasos90@gmail.com>
2016-11-25 21:10:01 +0100

    Dropdowns excluding storages which can not be assigned

--- routes/index.js
+++ routes/index.js
@@ -17,12 +17,16 @@ let render = (res) => {
         console.log("---------------------------");
         console.log(unassignedStorages);
         console.log("=======");
+        let assignedIds = getAssignedIds(assignedStorages);
+        console.log(assignedIds);
+        console.log("!!!!!!!");
 
         Storage.find({}, (err, allStorages) => {
 
             res.render("index", {
                 title: "Express",
                 allStorages: allStorages,
+                assignedIds: assignedIds,
                 assignedStorages: assignedStorages,
                 unassignedStorages: unassignedStorages
             });
@@ -62,6 +66,16 @@ let render = (res) => {
         }
         console.log("New storage item inserted:", insertedItem);
     });*/
+};
+
+let getAssignedIds = (hierarchy) => {
+
+    let result = [];
+    for (let relation of hierarchy) {
+        result.push(relation.id);
+        result.push(relation.targetStorage.id);
+    }
+    return result;
 };
 
 module.exports = router;
